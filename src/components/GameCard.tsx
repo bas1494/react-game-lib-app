569d683b5207f54f4bafab02a9f63773d82fba03
Bas Fonville <basfonville17@gmail.com>
2024-07-31 15:11:46 +0200

    Image url service for game images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,5 @@
 import { Game } from "../models/games";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -7,8 +8,15 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <div className="card">
+      <img
+        src={getCroppedImageUrl(game.background_image)}
+        className="card-img-top"
+        alt={game.background_image}
+      ></img>
       <div className="card-body">
-        <h5 className="card-title">{game.name}</h5>
+        <h5 className="card-title text-truncate">{game.name}</h5>
+        <p>Rating</p>
+        <p>Rating</p>
       </div>
     </div>
   );
